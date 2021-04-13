f802559b4d070fe0e258eefe17fed64dcf3a26ad
A-Morgan47 <80923478+A-Morgan47@users.noreply.github.com>
2021-04-13 13:41:20 -0700

    Update playerMoves.c

--- playerMoves.c
+++ playerMoves.c
@@ -6,11 +6,11 @@ int ID=1;
 
 void printMove(char c){        
    if (c=='s'||c=='w'||c=='f'){
-   FILE * f1=fopen("moves.txt", "w");
-   fprintf(f1, "%c%d", c, ID);
-   ID+=1;
-   fclose(f1);
-   printf("In Printmove: %c%d\n", c, ID);
+      FILE * f1=fopen("moves.txt", "w");
+      fprintf(f1, "%c%d", c, ID);
+      ID+=1;
+      fclose(f1);
+      printf("In Printmove: %c%d\n", c, ID);
    }
 }
 
@@ -19,13 +19,15 @@ void printMove(char c){
 int main(){
    char v; 
    //scanf("%c", &v);
+      
    while (v!='o'){      
-   scanf("%c", &v);
-   printf("V= %c\n", v);   
-   if (v=='o'){
-   return 0;
-   }
-   printMove(v);
-   }
-   return 0;
+         scanf("%c", &v);
+         printf("V= %c\n", v);   
+            if (v=='o'){
+               return 0;
+            }
+         printMove(v);
+      }
+
+return 0;
 }
