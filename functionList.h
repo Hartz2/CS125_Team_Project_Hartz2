048f0a7fe7f6391ffc0bb28e86da8c434345085c
A-Morgan47 <80923478+A-Morgan47@users.noreply.github.com>
2021-04-13 13:38:29 -0700

    Update functionList.h

--- functionList.h
+++ functionList.h
@@ -1,18 +1,22 @@
 #define ROWS 16
 #define COLS 57
-void collide(int, int);
+
 char beginKeyReading();
+void collide(int, int);
 void clearScreen();
 void printTower();
-char wreckage;
 void printArray(char lines[ROWS][COLS]);
+
+char wreckage;
 int loop;
 int spd;
 int points;
 int ID;
+
 extern int c;
 extern int pos;
 extern int playerPosition;
+
 /*char tower[ROWS][COLS]={
      "          ____________________________________________ ",
      "                                                     | ",
